ba03f4ef59052575950931e23e95ef926808993b
Frohani <frohani@mail.csuchico.edu>
2017-12-04 13:15:08 -0500

    Add files via upload

--- main.m
+++ main.m
@@ -1,12 +1,12 @@
 function main
-% performs all the optimizations and produces the plots and tables
+    % performs all the optimizations and produces the plots and tables
 	close all
-    global result problem
-    
-	problem.N = 70;			% number of collocation points
+    global result problem 
+	problem.N = 70;			        % number of collocation points
     problem.discretization = 'BE';  % Backward Euler(BE) or Midpoint Euler(ME) discretization
+    problem.tracking = 3;           % if 1 tracking the angles only else track all states (but flywheel velocity)
     problem.Wtrack = 1; 
-    problem.Weffort = 0;
+    problem.Weffort = 25;
     result = optimize(problem);
     save 'rowingtest' result
 end
