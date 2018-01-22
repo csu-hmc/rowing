95fc79ee7a32fc9342592a4e459e8708f1f9c575
Frohani <frohani@mail.csuchico.edu>
2018-01-22 07:42:07 -0500

    Predictive Simulation
    Only effort is being used and no tracking. The time in which cable is in
    its maximum length is being optimized as one extra optimization
    parameter.

--- main.m
+++ main.m
@@ -1,12 +1,17 @@
 function main
     % performs all the optimizations and produces the plots and tables
 	close all
+    clc
+    clear
     global result problem 
 	problem.N = 70;			        % number of collocation points
+%     model.task.Lmin = 0.2077;       % min and max of cable lenght from the data
+%     model.Lmax.Lmin =  1.1288;
     problem.discretization = 'BE';  % Backward Euler(BE) or Midpoint Euler(ME) discretization
+    problem.cabletask = 1;
     problem.tracking = 3;           % if 1 tracking the angles only else track all states (but flywheel velocity)
-    problem.Wtrack = 1; 
-    problem.Weffort = 25;
+    problem.Wtrack = 0; 
+    problem.Weffort = 60;
     result = optimize(problem);
     save 'rowingtest' result
 end
